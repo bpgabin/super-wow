fda30e342038a95f21dc6cf161fb83d360141d09
Brian Gabin <bpgabin@gmail.com>
2013-11-21 01:43:51 -0800

    Initial Commit with Starter Scene
    Setup a new 2D Unity project and set up a simple realistic Earth to have in the center.

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+    // Test
+    public Vector3 speed;
+
+	void Update () {
+        transform.Rotate(speed * Time.deltaTime);
+    }
+}
